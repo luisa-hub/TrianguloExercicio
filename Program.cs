b8abe4423fe2d6ff8b99a4bad56e173eb433840a
Luisa Farias <luisafariasdemacedo@gmail.com>
2021-05-10 15:16:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Equilátero
+            Triangulo triangulo = new Triangulo();
+
+            triangulo.ladoA = 2;
+            triangulo.ladoB = 2;
+            triangulo.ladoC = 2;
+
+            triangulo.VerificarTipoTriangulo();
+
+            //Isosceles
+            Triangulo trianguloIsosceles = new Triangulo();
+
+            trianguloIsosceles.ladoA = 3;
+            trianguloIsosceles.ladoB = 3;
+            trianguloIsosceles.ladoC = 4;
+
+            trianguloIsosceles.VerificarTipoTriangulo();
+
+
+            // Escaleno
+            Triangulo trianguloEscaleno = new Triangulo();
+
+            trianguloEscaleno.ladoA = 5;
+            trianguloEscaleno.ladoB = 3;
+            trianguloEscaleno.ladoC = 4;
+
+            trianguloEscaleno.VerificarTipoTriangulo();
+
+            //TrianguloInvalido
+
+            Triangulo trianguloInvalido = new Triangulo();
+
+            trianguloInvalido.ladoA = 5;
+            trianguloInvalido.ladoB = 2;
+            trianguloInvalido.ladoC = 1;
+
+            trianguloInvalido.VerificarTipoTriangulo();
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloExercicio
+{
+    class Triangulo
+    {
+        public double ladoA, ladoB, ladoC;
+        private bool tipoTriangulo;
+
+        
+        
+        public void VerificarTipoTriangulo() {
+
+            if (!EhInvalido())
+            {
+                if (Equilatero())
+                    Console.WriteLine("O triângulo é equilátero");
+
+                if (Isosceles())
+                    Console.WriteLine("O triângulo é isósceles");
+
+                if (Escaleno())
+                    Console.WriteLine("O triângulo é escaleno");
+            }
+        
+        }
+
+        private bool EhInvalido()
+        {
+
+            bool invalido = true;
+
+            if ((ladoA < ladoB + ladoC) && (ladoB < ladoA + ladoC) && (ladoC < ladoB + ladoA))
+            {
+
+                invalido = false;
+
+            }
+
+            else
+            {
+                Console.WriteLine("Lados do triângulo são inválidos");
+            }
+
+            return invalido;
+        }
+
+
+        private bool Equilatero() {
+
+            tipoTriangulo = false;
+
+            if (ladoA == ladoB && ladoB == ladoC) {
+
+                tipoTriangulo = true;
+            }
+
+            return tipoTriangulo;
+
+        }
+
+        private bool Isosceles() {
+
+            tipoTriangulo = false;
+
+            if ((ladoA == ladoB && ladoB != ladoC) || (ladoB == ladoC && ladoC != ladoA) || (ladoC == ladoA && ladoA != ladoB)) {
+
+                tipoTriangulo = true;
+            }
+
+            return tipoTriangulo;
+        }
+
+       private bool Escaleno() {
+
+            tipoTriangulo = false;
+
+            if (ladoA != ladoB && ladoB != ladoC && ladoA != ladoC) {
+
+                tipoTriangulo = true;
+
+            }
+
+            return tipoTriangulo;
+        
+        }
+
+    }
+}
